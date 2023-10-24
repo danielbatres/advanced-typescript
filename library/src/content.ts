495b9e0ce615b2b347821f0fb2c292524e335daf
danielbatres <danielbatresuser@gmail.com>
2023-10-23 19:45:06 -0600

    Alias: base code

---
+++ library/src/content.ts
@@ -0,0 +1,28 @@
+import { ComplicatedJoke, Joke } from './interfaces/joke.interface';
+import { Quote } from './interfaces/quote.interface';
+import { Riddle } from './interfaces/riddle.interface';
+
+export const quote: Quote = {
+  quote:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  author: '@batres',
+};
+
+export const joke: Joke = {
+  body:
+    'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  category: 'simple joke',
+};
+
+export const hardJoke: ComplicatedJoke = {
+  body:
+    'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  category: 'simple joke',
+  level: 3,
+};
+
+export const riddle: Riddle = {
+  riddle:
+    'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+  answer: 'Search loremipsum generator',
+};
