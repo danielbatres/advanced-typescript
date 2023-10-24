25f0d2e5053ee494e91168c3f26365407adc2e58
danielbatres <danielbatresuser@gmail.com>
2023-10-23 21:40:20 -0600

    Using and packaging TypeScript project with jQuery

--- app/src/init.ts
+++ app/src/init.ts
@@ -1,7 +1,10 @@
-import { makeA } from "library-advanced-typescript-course";
-export { makeA as picker };
 import $ from 'jquery';
+import { makeA } from "library-advanced-typescript-course";
+import { Main } from "./main";
+export { makeA as picker };
 
 $(() => {
   console.log("Document ready to use jQuery");
+
+  const main = new Main();
 });
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,35 @@
+import $ from 'jquery';
+import { Joke, Quote } from 'library-advanced-typescript-course/dist/interfaces';
+import { getAllQuotes } from 'library-advanced-typescript-course/dist/utils/quotes';
 import './init';
 import { picker } from './init';
 
-const resultJoke = picker("joke");
+export class Main {
+  constructor() {
+    const resultJoke = picker("joke") as Joke;
 
-console.log(resultJoke);
+    this.displayJoke(resultJoke);
+    this.displayQuotes();
+  }
+
+  displayJoke(joke: Joke) {
+    $('.content').text(joke.body);
+    $('.source').text(joke.category);
+  }
+
+  displayQuotes() {
+    const quotes: Quote[] = getAllQuotes();
+
+    quotes.forEach((quote: Quote, index: number) => {
+      console.log(quote);
+      $('.quotes').append(
+        `<div>
+          <blockquote>
+            <strong>${index + 1}:</strong> ${quote.quote}
+          </blockquote>
+          <em>${quote.author}</em>
+        </div>`
+      )
+    })
+  }
+}
