aacef6450fc26f3e9b81875d7489904e59e88d57
danielbatres <danielbatresuser@gmail.com>
2023-10-24 11:21:36 -0600

    Combining objects with generic aliases

--- library/src/interfaces/common.types.ts
+++ library/src/interfaces/common.types.ts
@@ -6,14 +6,19 @@ import { Riddle } from './riddle.interface';
 /** Fact types per kind of content */
 //export type Fact = Quote | Quote[] | Joke<string> | Riddle | string;
 
-export type Fact<T> = T;
+export type Fact<T, U = {}> = T | U;
 
 const quote: Fact<Quote> = {
   author: "batres",
   quote: "Happy Coding"
 }
 
-
+const mixedFact: Fact<Quote, Riddle> = {
+  author: "batres",
+  quote: "Happy Coding",
+  riddle: "Wich fish costs the most?",
+  answer: "A goldfish"
+}
 
 /* const riddle: Fact<Joke<string>> = {
 
