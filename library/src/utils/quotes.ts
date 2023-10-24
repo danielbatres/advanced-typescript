002cda6877bd323ffb7434f64cf83a076b08807c
danielbatres <danielbatresuser@gmail.com>
2023-10-23 19:51:05 -0600

    Default exports base code

--- library/src/utils/quotes.ts
+++ library/src/utils/quotes.ts
@@ -1,10 +1,14 @@
+import { quote } from "../content";
+import { Quote } from "../interfaces/quote.interface";
+
 /** Get a random quote from a base JSON file */
-export default function getARandomQuote(): unknown {
-  const quote: unknown = {};
+export default function getARandomQuote(): Quote {
   return quote;
 }
 
 /** Get all quotes from a base JSON file */
-export function getAllQuotes(): unknown {
-  return null;
+export function getAllQuotes(): Quote[] {
+  const allQuotes: Quote[] = [quote, quote, quote, quote, quote]
+
+  return allQuotes;
 }
