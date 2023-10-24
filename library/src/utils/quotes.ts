fa96df0893e3de6bd4e142eaa33223d49847a38d
danielbatres <danielbatresuser@gmail.com>
2023-10-23 20:18:57 -0600

    Data types for third party modules

--- library/src/utils/quotes.ts
+++ library/src/utils/quotes.ts
@@ -1,9 +1,14 @@
 import { quote } from "../content";
 import { Quote } from "../interfaces/quote.interface";
 import * as content from "../assets/content.json";
+import * as _ from "lodash";
 
 /** Get a random quote from a base JSON file */
 export default function getARandomQuote(): Quote {
+  const randomNumber = _.random(0, content.quotes.length);
+  console.log(`Selection ${randomNumber}`);
+  const quote: Quote = content.quotes[randomNumber];
+
   return quote;
 }
 
